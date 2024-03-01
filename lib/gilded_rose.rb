f8d8e01a372ac6a02074562488a021cfdec06678
Vivian Ly <viv.ly.2803@gmail.com>
2024-02-29 21:52:46 -0600

    task conjured

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,6 @@
 module GildedRose
 
+
   class Item
     attr_reader :quality, :days_remaining
     def initialize(quality, days_remaining)
@@ -40,6 +41,18 @@ module GildedRose
       @quality += 1
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
+    end
+  end
+
+
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
     end
   end
 
