bb40caa56652457628e37f23c69b0ea3fdf64cd1
Vivian Ly <viv.ly.2803@gmail.com>
2024-02-29 19:22:31 -0600

    backstage_tick created 0 failures

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,6 +15,8 @@ class GildedRose
       return brie_tick
     when 'Sulfuras, Hand of Ragnaros'
       return sulfuras_tick
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return backstage_tick
     end
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
@@ -77,7 +79,19 @@ class GildedRose
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end
+
   def sulfuras_tick
 
   end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return    if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+
 end
