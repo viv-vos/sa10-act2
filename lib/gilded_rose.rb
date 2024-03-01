210e9da9083801e7e071b392d56c568628e6fb80
Vivian Ly <viv.ly.2803@gmail.com>
2024-02-29 21:38:33 -0600

    deleted class Normal and Brie

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,43 +10,20 @@ module GildedRose
     when 'Aged Brie'
       Brie
     when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
+      Item
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
     end
   end
 
   class Item
-    attr_reader :quality, :days_remaining
 
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  end
-
-
-
-  class Normal < Item
-    def tick
-      @days_remaining -= 1
-      return if @quality == 0
-
-      @quality -= 1
-      @quality -= 1 if @days_remaining <= 0
-    end
-  end
-
-
-  class Brie < Item
 
     def tick
-      @days_remaining -= 1
-      return if @quality >= 50
 
-      @quality += 1
-      @quality += 1 if @days_remaining <= 0
     end
   end
+
 
 
   class Sulfuras < Item
