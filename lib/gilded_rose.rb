1550060b5cd9f7b5e40fd032b80ad18a32ec32a7
Vivian Ly <viv.ly.2803@gmail.com>
2024-02-29 19:02:18 -0600

    normal item tick 0 failures

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -57,4 +57,16 @@ class GildedRose
     end
   end
 
+  def normal_item_tick
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -=1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
+    end
+    @days_remaining -= 1
+  end
+
 end
