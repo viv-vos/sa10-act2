a9449f2035b076d02bccd61d6917f67abe5c7119
Vivian Ly <viv.ly.2803@gmail.com>
2024-02-29 21:50:55 -0600

    Conclusion issues fixed

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,20 +1,32 @@
 module GildedRose
 
-  DEFAULT_CLASS = Item
-  SPECIALIZED_CLASSES = {'Normal Item' => Normal,
-  'Aged Brie' => Brie,
-  'Backstage passes to a TAFKAL80ETC concert' => Backstage}
-
-
-  def self.new(name, days_remaining, quality)
-    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
-  end
-
   class Item
-
-
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
     def tick
 
+    end
+  end
+
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0
     end
   end
 
@@ -31,8 +43,12 @@ module GildedRose
     end
   end
 
-  class Normal
-  class Brie
-  class Sulfuras
-  class Backstage
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {'Normal Item' => Normal,
+  'Aged Brie' => Brie,
+  'Backstage passes to a TAFKAL80ETC concert' => Backstage}
+
+  def self.new(name, days_remaining, quality)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
 end
